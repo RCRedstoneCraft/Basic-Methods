e5cd18c7db51c10171511416ddccb7a7c1b40e35
RedstoneCraft_ <marvinprandner@gmail.com>
2025-12-02 16:12:36 +0100

    array list ai

--- src/Methods/NewArrayList.java
+++ src/Methods/NewArrayList.java
@@ -1,4 +1,37 @@
 package Methods;
 
-public class NewArrayList {
+public class NewArrayList<T> {
+    private T[] data;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public NewArrayList() {
+        data = (T[]) new Object[10]; // interne Speicherung
+        size = 0;
+    }
+
+    public void add(T element) {
+        if (size == data.length) {
+            resize();
+        }
+        data[size++] = element;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return data[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        T[] newData = (T[]) new Object[data.length * 2];
+        System.arraycopy(data, 0, newData, 0, data.length);
+        data = newData;
+    }
 }
