2d2f9abfa17ce0a54a8bcdfe6ce49af37125e52e
leon-stroebitzer <leon.stroebitzer@gmail.com>
2025-12-02 16:15:59 +0100

    Main
    Aufrufe

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,13 @@ public class Main {
         Methods.NewMath.random(5, 10);
         Methods.NewMath.random(5, 10);
     }
+
+    Methods.NewArrayList<String> namen = new Methods.NewArrayList<>();
+    namen.add("Max");
+    namen.add("Anna");
+
+    Methods.NewArrayList<Integer> zahlen = new Methods.NewArrayList<>();
+    zahlen.add(1);
+    zahlen.add(42);
+
 }
