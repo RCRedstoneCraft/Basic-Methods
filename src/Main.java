4b8942bcee1c7dc218f32c6ce6bc1b6628fdb5bd
leon-stroebitzer <leon.stroebitzer@gmail.com>
2025-12-02 16:17:54 +0100

    Fehlerbehebung

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@ public class Main {
     static void main() {
         Methods.NewMath.random(5, 10);
         Methods.NewMath.random(5, 10);
+
+        Methods.NewArrayList<String> namen = new Methods.NewArrayList<>();
+        namen.add("Max");
+        namen.add("Anna");
+
+        Methods.NewArrayList<Integer> zahlen = new Methods.NewArrayList<>();
+        zahlen.add(1);
+        zahlen.add(42);
     }
-
-    Methods.NewArrayList<String> namen = new Methods.NewArrayList<>();
-    namen.add("Max");
-    namen.add("Anna");
-
-    Methods.NewArrayList<Integer> zahlen = new Methods.NewArrayList<>();
-    zahlen.add(1);
-    zahlen.add(42);
-
 }
