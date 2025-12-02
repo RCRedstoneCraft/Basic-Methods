a474364b17b7ab586edd45be8687ccf07c7ba6f0
RedstoneCraft_ <marvinprandner@gmail.com>
2025-12-02 16:36:58 +0100

    Update NewMath.java

--- src/Methods/NewMath.java
+++ src/Methods/NewMath.java
@@ -26,4 +26,24 @@ public class NewMath {
         return min;
     }
 
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int k = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > k) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = k;
+        }
+    }
+
+    public static double getHypotenuse(double a, double b) {
+        return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    }
+
+    
 }
