0d8ac2dce1cbb340665f23ed371250385695a7aa
RedstoneCraft_ <marvinprandner@gmail.com>
2025-12-02 16:32:43 +0100

    Update NewMath.java

--- src/Methods/NewMath.java
+++ src/Methods/NewMath.java
@@ -2,21 +2,10 @@ package Methods;
 
 public class NewMath {
 
-    /**
-     *
-     * @param min minimum (inclusive)
-     * @param max maximum (inclusive)
-     * @return random number min - max
-     */
     public static int random(int min, int max) {
         return (int) (Math.random() * (max - min + 1)) + min;
     }
 
-    /**
-     *
-     * @param arr array
-     * @return maximum of the array
-     */
     public static int getMax(int[] arr) {
         int max = arr[0];
         for (int i = 1; i < arr.length; i++) {
@@ -27,11 +16,6 @@ public class NewMath {
         return max;
     }
 
-    /**
-     *
-     * @param arr array
-     * @return minimum of the array
-     */
     public static int getMin(int[] arr) {
         int min = arr[0];
         for (int i = 1; i < arr.length; i++) {
