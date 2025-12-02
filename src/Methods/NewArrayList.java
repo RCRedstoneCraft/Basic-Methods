2db7a7ff9f56163bafd1db9f4cb7204b75433efd
leon-stroebitzer <leon.stroebitzer@gmail.com>
2025-12-02 16:24:43 +0100

    Update NewArrayList.java

--- src/Methods/NewArrayList.java
+++ src/Methods/NewArrayList.java
@@ -2,46 +2,20 @@ package Methods;
 
 public class NewArrayList<T> {
     private T[] data;
-    private int size;
 
-    @SuppressWarnings("unchecked")
     public NewArrayList() {
-        data = (T[]) new Object[10]; // interne Speicherung
-        size = 0;
+        data = (T[]) new Object[0]; // interne Speicherung
     }
 
-    /**
-     *
-     * @param element adds the element to the array list
-     */
     public void add(T element) {
-        if (size == data.length) {
-            resize();
-        }
-        data[size++] = element;
+        resize();
+        data[data.length] = element;
     }
 
-    /**
-     *
-     * @param index index
-     * @return the item at the index
-     */
     public T get(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
-        }
         return data[index];
     }
 
-    /**
-     *
-     * @return size of the array list
-     */
-    public int size() {
-        return size;
-    }
-
-    @SuppressWarnings("unchecked")
     private void resize() {
         T[] newData = (T[]) new Object[data.length * 2];
         System.arraycopy(data, 0, newData, 0, data.length);
